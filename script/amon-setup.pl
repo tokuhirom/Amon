77addb7d6b668db4d45200af93f510f223671935
tokuhirom <tokuhirom@gmail.com>
2009-12-20 22:50:46 +0900

    more better impl

--- script/amon-setup.pl
+++ script/amon-setup.pl
@@ -7,63 +7,64 @@ use Pod::Usage;
 use Text::MicroTemplate ':all';
 
 my $perlver = $] >= 5.010000 ? '5.10' : '5.8';
+our $module;
 GetOptions(
-    'perlver=s' => \$perlver,
-    'help' => \my $help,
+'perlver=s' => \$perlver,
+'help' => \my $help,
 ) or pod2usage(0);
 pod2usage(1) if $help;
 
 my $confsrc = <<'...';
--- lib/$name.pm
-package $name;
+-- lib/$path.pm
+package [%= $module %];
 use Amon;
 1;
--- lib/$name/Web.pm
-package $name::Web;
+-- lib/$path/Web.pm
+package [%= $module %]::Web;
 use Amon::Web (
-  view_class => 'MT',
+view_class => 'MT',
 );
 1;
--- lib/$name/V/MT/Context.pm
-package $name::V::MT::Context;
+-- lib/$path/V/MT/Context.pm
+package [%= $module %]::V::MT::Context;
 use Amon::V::MT::Context;
 1;
--- lib/$name/Web/Dispatcher.pm
+-- lib/$path/Web/Dispatcher.pm
 % my $perlver = shift;
-package $name::Web::Dispatcher;
+package [%= $module %]::Web::Dispatcher;
 use Amon::Web::Dispatcher;
 % if ($perlver eq '5.10') {
 use feature 'switch';
 
 sub dispatch {
-    my ($class, $req) = @_;
-    given ($req->path_info) {
-        when ('/') {
-            call("Root", 'index');
-        }
-        default {
-            res_404();
-        }
+my ($class, $req) = @_;
+given ($req->path_info) {
+    when ('/') {
+        call("Root", 'index');
     }
+    default {
+        res_404();
+    }
+}
 }
 % } else {
 sub dispatch {
-    my ($class, $req) = @_;
-    if ($req->path_info eq '/') {
-        call("Root", 'index');
-    } else {
-        res_404();
-    }
+my ($class, $req) = @_;
+if ($req->path_info eq '/') {
+    call("Root", 'index');
+} else {
+    res_404();
+}
 }
 % }
 
 1;
--- lib/$name/Web/C/Root.pm
-package $name::Web::C::Root;
+-- lib/$path/Web/C/Root.pm
+package [%= $module %]::Web::C::Root;
 use Amon::Web::C;
 
 sub index {
-    render("index.mt");
+render("index.mt");
 }
 
 1;
@@ -74,51 +75,60 @@ sub index {
 -- tmpl/base.mt
 <!doctype html>
 <html>
-  <head>
-    <title><? block title => 'Amon' ?></title>
-  </head>
-  <body>
-    <? block content => 'body here' ?>
-  </body>
+<head>
+<title><? block title => 'Amon' ?></title>
+</head>
+<body>
+<? block content => 'body here' ?>
+</body>
 </html>
--- $name.psgi
-use $name;
-use $name::Web;
-$name::Web->app("./");
+-- $dist.psgi
+use [%= $module %];
+use [%= $module %]::Web;
+[%= $module %]::Web->app("./");
 ...
 
 &main;exit;
 
 sub _mkpath {
-    my $d = shift;
-    print "mkdir $d\n";
-    mkpath $d;
+my $d = shift;
+print "mkdir $d\n";
+mkpath $d;
 }
 
 sub main {
-    my $name = shift @ARGV or pod2usage(0);
-    (my $distname = $name) =~ s/::/-/g;
-    mkdir $distname or die $!;
-    chdir $distname or die $!;
-    _mkpath "lib/$name/";
-    _mkpath "lib/$name/V";
-    _mkpath "lib/$name/V/MT";
-    _mkpath "lib/$name/Web/C";
-    _mkpath "lib/$name/M";
-    _mkpath "tmpl";
-    _mkpath "t";
+$module = shift @ARGV or pod2usage(0);
+$module =~ s!-!::!g;
 
-    my $conf = _parse_conf($confsrc);
-    while (my ($file, $tmpl) = each %$conf) {
-        $file =~ s/(\$\w+)/$1/gee;
-        $tmpl =~ s/(\$name)/$1/gee;
-        my $code = Text::MicroTemplate->new(
-            tag_start => '[%',
-            tag_end   => '%]',
-            line_start => '%',
-            template => $tmpl,
-        )->build;
-        my $res = $code->($perlver)->as_string;
+# $module = "Foo::Bar"
+# $dist   = "Foo-Bar"
+# $path   = "Foo/Bar"
+my @pkg  = split /::/, $module;
+my $dist = join "-", @pkg;
+my $path = join "/", @pkg;
+
+mkdir $dist or die $!;
+chdir $dist or die $!;
+_mkpath "lib/$path";
+_mkpath "lib/$path/V";
+_mkpath "lib/$path/V/MT";
+_mkpath "lib/$path/Web/C";
+_mkpath "lib/$path/M";
+_mkpath "tmpl";
+_mkpath "t";
+
+my $conf = _parse_conf($confsrc);
+while (my ($file, $tmpl) = each %$conf) {
+    $file =~ s/(\$\w+)/$1/gee;
+    my $code = Text::MicroTemplate->new(
+        tag_start => '[%',
+        tag_end   => '%]',
+        line_start => '%',
+        template => $tmpl,
+    )->code;
+    my $sub = eval "package main;our \$module; sub { Text::MicroTemplate::encoded_string(($code)->(\@_))}";
+    die $@ if $@;
+        my $res = $sub->($perlver)->as_string;
 
         print "writing $file\n";
         open my $fh, '>', $file or die "Can't open file($file):$!";
@@ -146,4 +156,10 @@ __END__
 =head1 SYNOPSIS
 
     % amon-setup.pl MyApp
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno
+
+=cut
 
