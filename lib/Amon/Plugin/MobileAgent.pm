04debb54a4624c563794b8ea021bf4686b61dad7
tokuhirom <tokuhirom@gmail.com>
2010-01-05 10:54:39 +0900

    updated docs

--- lib/Amon/Plugin/MobileAgent.pm
+++ lib/Amon/Plugin/MobileAgent.pm
@@ -16,3 +16,31 @@ sub init {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+Amon::Plugin::MobileAgent - HTTP::MobileAgent plugin for Amon
+
+=head1 SYNOPSIS
+
+    package MyApp::Web;
+    use Amon::Web -base => (
+        default_view_class => 'MT',
+    );
+    __PACKAGE__->load_plugins(
+        qw/ MobileAgent /
+    );
+    1;
+
+=head1 DESCRIPTION
+
+Integrate with L<HTTP::MobileAgent>
+
+=head1 SEE ALSO
+
+L<HTTP::MobileAgent>, L<Amon>
+
+
+=cut
+
