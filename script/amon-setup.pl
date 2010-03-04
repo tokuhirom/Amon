703eb0ec4f73e1c076c0356cbcaa5a30214b697a
tokuhirom <tokuhirom@gmail.com>
2010-03-04 22:49:52 +0900

    make $dist as default title/header

--- script/amon-setup.pl
+++ script/amon-setup.pl
@@ -53,14 +53,14 @@ sub index {
 1;
 -- tmpl/index.mt
 ? extends 'base.mt';
-? block title => 'amon page';
+? block title => '[%= $dist %] page';
 ? block content => sub { 'hello, Amon world!' };
 -- tmpl/base.mt
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title><? block title => 'Amon' ?></title>
+    <title><? block title => '[%= $dist %]' ?></title>
     <meta http-equiv="Content-Style-Type" content="text/css" />  
     <meta http-equiv="Content-Script-Type" content="text/javascript" />  
     <link href="<?= uri_for('/static/css/main.css') ?>" rel="stylesheet" type="text/css" media="screen" />
