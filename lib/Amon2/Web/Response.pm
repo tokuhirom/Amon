1ed22aa69abacad5284698d59090a1582dd25cc6
tokuhirom <tokuhirom@gmail.com>
2010-08-08 01:51:50 +0900

    updated docs

--- lib/Amon2/Web/Response.pm
+++ lib/Amon2/Web/Response.pm
@@ -11,80 +11,11 @@ __END__
 
 Amon2::Web::Response - web response class for Amon2
 
-=head1 SYNOPSIS
-
-    my $res = Amon2::Web::Response->new(200, ['Content-Type' => 'text/plain'], 'OK');
-    $res->finalize;
-
 =head1 DESCRIPTION
 
 This is response class for Amon2.
 
-=head1 METHODS
-
-=over 4
-
-=item new
-
-  $res = Amon2::Web::Response->new;
-  $res = Amon2::Web::Response->new($status);
-  $res = Amon2::Web::Response->new($status, $headers);
-  $res = Amon2::Web::Response->new($status, $headers, $body);
-
-Creates a new Amon2::Web::Response object.
-
-=item status
-
-  $res->status(200);
-  $status = $res->status;
-
-Sets and gets HTTP status code. C<code> is an alias.
-
-=item headers
-
-  $headers = $res->headers;
-  $res->headers([ 'Content-Type' => 'text/html' ]);
-  $res->headers({ 'Content-Type' => 'text/html' });
-  $res->headers( HTTP::Headers->new );
-
-Sets and gets HTTP headers of the response. Setter can take either an
-array ref, a hash ref or L<HTTP::Headers> object containing a list of
-headers.
-
-=item body
-
-  $res->body($body_str);
-  $res->body([ "Hello", "World" ]);
-  $res->body($io);
-
-Gets and sets HTTP response body. Setter can take either a string, an
-array ref, or an IO::Handle-like object. C<content> is an alias.
-
-=item header
-
-  $res->header('X-Foo' => 'bar');
-  my $val = $res->header('X-Foo');
-
-Shortcut for C<< $res->headers->header >>.
-
-=item content_type, content_length, content_encoding
-
-  $res->content_type('text/plain');
-  $res->content_length(123);
-  $res->content_encoding('gzip');
-
-Shortcut for the equivalent get/set methods in C<< $res->headers >>.
-
-=item redirect
-
-  $res->redirect($url);
-  $res->redirect($url, 301);
-
-Sets redirect URL with an optional status code, which defaults to 302.
-
-=item location
-
-Gets and sets C<Location> header.
+This class is child class of L<Plack::Response>.
 
 =back
 
