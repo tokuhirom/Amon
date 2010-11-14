2a8f545fa58c396e0286dc665e9ba3b4ef45fdb9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-11-14 10:13:45 +0900

    Checking in changes prior to tagging of version 2.01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 44a42c2..c304fd1 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,11 @@
    +2.01
    +
    +    - fixed test case
    +    - added second argument for: $c->redirect($location, \%params)
    +    - added second argument for: $c->redirect($location, \@params)
    +    - added $c->fillin_form() method to Amon2;:Plugin::Web:FillInForm and
    +      Amon2::Plugin::Web::FillInForm.
    +
     2.00
    
         - refactored

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -7,7 +7,7 @@ use Plack::Util ();
 use Data::OptList ();
 use Carp ();
 
-our $VERSION = '2.00';
+our $VERSION = '2.01';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
