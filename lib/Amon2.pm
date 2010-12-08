fb64e5ee1431033a0663687bc6b4926fb588371f
tokuhirom <tokuhirom@gmail.com>
2010-12-08 17:08:40 +0900

    Checking in changes prior to tagging of version 2.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ca4cac5..08fcf20 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,8 @@
    +2.06
    +
    +    - fixed csrfdefender for amon2 style(before this commit, it still amon1 style!)
    +      (tokuhirom)
    +
     2.05
    
         - Plugin::Web::CSRFDefender: oops. older version does not works without Amon1

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -7,7 +7,7 @@ use Plack::Util ();
 use Data::OptList ();
 use Carp ();
 
-our $VERSION = '2.05';
+our $VERSION = '2.06';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
