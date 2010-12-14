ac3646c9fdac56d79053955d305c4627e7441f5b
tokuhirom <tokuhirom@gmail.com>
2010-12-14 13:09:49 +0900

    Checking in changes prior to tagging of version 2.09.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index eb64946..e000c22 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,7 @@
    +2.09
    +
    +    - first release for CPAN
    +
     2.08
    
         - Web::Dispatcher::RouterSimple: export 'router' method

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -7,7 +7,7 @@ use Plack::Util ();
 use Data::OptList ();
 use Carp ();
 
-our $VERSION = '2.08';
+our $VERSION = '2.09';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
