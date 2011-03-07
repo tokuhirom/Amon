43eb97982dbe75e4394e850f76a568977cfe4765
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-03-07 10:38:24 +0900

    Implement default load_config()

--- lib/Amon2/Setup/Flavor/Basic.pm
+++ lib/Amon2/Setup/Flavor/Basic.pm
@@ -23,9 +23,6 @@ use parent qw/Amon2/;
 our $VERSION='0.01';
 use 5.008001;
 
-use Amon2::Config::Simple;
-sub load_config { Amon2::Config::Simple->load(shift) }
-
 # __PACKAGE__->load_plugin(qw/DBI/);
 
 1;
