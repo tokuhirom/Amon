f6331911267c18a2f6fee6d9e89fd11471c21925
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-05-14 11:27:41 +0900

    Checking in changes prior to tagging of version 2.34.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 9273d76..d3805ec 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,7 @@
    +2.34
    +
    +    - Switch to EU::MM from M::I at setup script. This is required for more easy installing.
    +
     2.33
    
         [INCOMPATIBLE CHANGE]

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Data::OptList ();
 use Carp ();
 use Amon2::Config::Simple;
 
-our $VERSION = '2.33';
+our $VERSION = '2.34';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
