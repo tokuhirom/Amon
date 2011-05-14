17d70ab903d8ea6646462dad0311a3f7531027eb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-05-14 11:04:28 +0900

    remove japanese mobile things from core

--- t/600_plugins/007_json.t
+++ t/600_plugins/007_json.t
@@ -1,7 +1,6 @@
 use strict;
 use warnings;
 use Test::More;
-use Test::Requires 'HTTP::MobileAgent';
 
 {
     package MyApp;
--- t/apps/Extended/lib/Extended/Web/Request.pm
+++ t/apps/Extended/lib/Extended/Web/Request.pm
@@ -2,11 +2,5 @@ package Extended::Web::Request;
 use strict;
 use warnings;
 use base qw/Amon2::Web::Request/;
-use HTTP::MobileAgent;
-
-sub mobile_agent {
-    my $self = shift;
-    $self->{mobile_agent} ||= HTTP::MobileAgent->new($self->headers);
-}
 
 1;
