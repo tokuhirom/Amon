fb004150ab30a0e5a94bec445a677ba1d8c0a3ee
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-07-11 05:45:38 +0900

    fixed docs

--- lib/Amon2/Web.pm
+++ lib/Amon2/Web.pm
@@ -184,7 +184,10 @@ Amon2::Web - Web Application Base.
 =head1 SYNOPSIS
 
     package MyApp;
-    use parent qw/Amon2::Web/;
+    use parent qw/Amon2/;
+
+    package MyApp::Web;
+    use parent qw/MyApp Amon2::Web/;
 
 =head1 DESCRIPTION
 
