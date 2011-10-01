2f0ad1b17252b8153d3664cb518bab4ef4a45501
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-02 02:05:00 +0900

    move xslate related settings to #xslate

--- lib/Amon2/Setup/Flavor/Base.pm
+++ lib/Amon2/Setup/Flavor/Base.pm
@@ -96,6 +96,40 @@ eval "use Test::Pod 1.00";
 plan skip_all => "Test::Pod 1.00 required for testing POD" if $@;
 all_pod_files_ok();
 
+@@ #xslate
+# setup view class
+use Text::Xslate;
+use File::Spec;
+{
+    my $view_conf = __PACKAGE__->config->{'Text::Xslate'} || +{ };
+    unless (exists $view_conf->{path}) {
+        $view_conf->{path} = [ File::Spec->catdir(__PACKAGE__->base_dir(), 'tmpl') ];
+    }
+    my $view = Text::Xslate->new(+{
+        'syntax'   => 'TTerse',
+        'module'   => [ 'Text::Xslate::Bridge::TT2Like' ],
+        'function' => {
+            c        => sub { Amon2->context() },
+            uri_with => sub { Amon2->context()->req->uri_with(@_) },
+            uri_for  => sub { Amon2->context()->uri_for(@_) },
+            static_file => do {
+                my %static_file_cache;
+                sub {
+                    my $fname = shift;
+                    my $c = Amon2->context;
+                    if (not exists $static_file_cache{$fname}) {
+                        my $fullpath = File::Spec->catfile($c->base_dir(), $fname);
+                        $static_file_cache{$fname} = (stat $fullpath)[9];
+                    }
+                    return $c->uri_for($fname, { 't' => $static_file_cache{$fname} });
+                }
+            },
+        },
+        %$view_conf
+    });
+    sub create_view { $view }
+}
+
 __END__
 
 =head1 NAME
--- lib/Amon2/Setup/Flavor/Minimum.pm
+++ lib/Amon2/Setup/Flavor/Minimum.pm
@@ -47,38 +47,7 @@ sub dispatch {
 : }
 
 : block create_view -> {
-# setup view class
-use Text::Xslate;
-use File::Spec;
-{
-    my $view_conf = __PACKAGE__->config->{'Text::Xslate'} || +{ };
-    unless (exists $view_conf->{path}) {
-        $view_conf->{path} = [ File::Spec->catdir(__PACKAGE__->base_dir(), 'tmpl') ];
-    }
-    my $view = Text::Xslate->new(+{
-        'syntax'   => 'TTerse',
-        'module'   => [ 'Text::Xslate::Bridge::TT2Like' ],
-        'function' => {
-            c        => sub { Amon2->context() },
-            uri_with => sub { Amon2->context()->req->uri_with(@_) },
-            uri_for  => sub { Amon2->context()->uri_for(@_) },
-            static_file => do {
-                my %static_file_cache;
-                sub {
-                    my $fname = shift;
-                    my $c = Amon2->context;
-                    if (not exists $static_file_cache{$fname}) {
-                        my $fullpath = File::Spec->catfile($c->base_dir(), $fname);
-                        $static_file_cache{$fname} = (stat $fullpath)[9];
-                    }
-                    return $c->uri_for($fname, { 't' => $static_file_cache{$fname} });
-                }
-            },
-        },
-        %$view_conf
-    });
-    sub create_view { $view }
-}
+: include "#xslate"
 : }
 
 : block load_plugins -> { }
