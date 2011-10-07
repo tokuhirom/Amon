15cc025abb2f9ad0edefe64eeaf44eb7e5b4b55a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-07 23:23:25 +0900

    one more test case

--- lib/Amon2/Setup/Flavor/Large.pm
+++ lib/Amon2/Setup/Flavor/Large.pm
@@ -247,6 +247,29 @@ test_psgi
         }
     };
 
+my $admin = Plack::Util::load_psgi 'admin.psgi';
+test_psgi
+    app => $admin,
+    client => sub {
+        my $cb = shift;
+
+        # 401
+        {
+            my $req = HTTP::Request->new(GET => "http://localhost/");
+            my $res = $cb->($req);
+            is($res->code, 401, 'basic auth');
+        }
+
+        # 200
+        {
+            my $req = HTTP::Request->new(GET => "http://localhost/");
+            $req->authorization_basic('admin', 'admin');
+            my $res = $cb->($req);
+            is($res->code, 200, 'basic auth');
+            like($res->content, qr{admin});
+        }
+    };
+
 done_testing;
 ...
 }
