ca948fdddfb23582bf2a252e2ae9fc3fa0c9b2fe
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-19 07:16:05 +0900

    do not use 5.10 feature in testing, for packaging reason

--- t/apps/DeepNamespace/lib/DeepNamespace/Web/User/Dispatcher.pm
+++ t/apps/DeepNamespace/lib/DeepNamespace/Web/User/Dispatcher.pm
@@ -1,16 +1,12 @@
 package DeepNamespace::Web::User::Dispatcher;
 use strict;
-use feature 'switch';
 
 sub dispatch {
     my ($class, $c) = @_;
-    given ($c->request->path_info) {
-        when ('/') {
-            return DeepNamespace::Web::User::C::Root->index($c);
-        }
-        default {
-            return res_404();
-        }
+    if ($c->request->path_info eq '/') {
+        return DeepNamespace::Web::User::C::Root->index($c);
+    } else {
+        return $c->res_404();
     }
 }
 
