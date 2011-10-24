e1abd3f77db8926fcdc448a4b912929b14038792
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-24 10:31:45 +0900

    Checking in changes prior to tagging of version 3.13.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c4a6306..2d60efe 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,8 @@
    +3.13
    +
    +    [MINOR FIX]
    +    - Config::Simple: better diag for loading configuration file
    +
     3.12
    
         [TESTING FIX]

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Data::OptList ();
 use Carp ();
 use Amon2::Config::Simple;
 
-our $VERSION = '3.12';
+our $VERSION = '3.13';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
