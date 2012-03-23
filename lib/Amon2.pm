974d5296428e43eeb8204878a7fb28a88d73e8f9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2012-03-23 17:50:40 +0900

    Checking in changes prior to tagging of version 3.33.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b7ee14c..1ffa980 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,7 @@
    +3.33
    +
    +    - fixed typo
    +
     3.32
    
         - lib/Amon2/Setup/Flavor/Large.pm: fixed typo (reported by pochy++)

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -7,7 +7,7 @@ use Plack::Util ();
 use Carp ();
 use Amon2::Config::Simple;
 
-our $VERSION = '3.32';
+our $VERSION = '3.33';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
