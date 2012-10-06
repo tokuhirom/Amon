c06db7697886a3c88d34603e0c7775904bf02cc2
tokuhirom <tokuhirom@gmail.com>
2012-10-06 15:28:49 +0900

    Checking in changes prior to tagging of version 3.54.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ed19d47..0501658 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,7 +1,9 @@
    -- jquery 1.8.0 => 1.8.2
    -- bootstrap v2.0.4 => v2.1.1
    -- add a context parameter ($c) to A::W::WebSocket::{on_error, on_eof}
    -  (hatyuki++)
    +3.54
    +
    +    - jquery 1.8.0 => 1.8.2
    +    - bootstrap v2.0.4 => v2.1.1
    +    - add a context parameter ($c) to A::W::WebSocket::{on_error, on_eof}
    +      (hatyuki++)
    
     3.53

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -7,7 +7,7 @@ use Plack::Util ();
 use Carp ();
 use Amon2::Config::Simple;
 
-our $VERSION = '3.53';
+our $VERSION = '3.54';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
