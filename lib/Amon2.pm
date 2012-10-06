0c52c4f9204aab926028d700567431f63e6712d6
tokuhirom <tokuhirom@gmail.com>
2012-10-06 16:03:05 +0900

    Checking in changes prior to tagging of version 3.55.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0501658..9478aff 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,8 @@
    +3.55
    +
    +    - use jshint instead of jsl, for testing application.
    +      in setup script.
    +
     3.54
    
         - jquery 1.8.0 => 1.8.2

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -7,7 +7,7 @@ use Plack::Util ();
 use Carp ();
 use Amon2::Config::Simple;
 
-our $VERSION = '3.54';
+our $VERSION = '3.55';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
