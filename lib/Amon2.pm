fcca36c0982983a9cbbd3b919d096394dd946769
tokuhirom <tokuhirom@gmail.com>
2012-12-15 12:25:45 +0900

    Checking in changes prior to tagging of version 3.66.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 585576d..695858c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,7 @@
    +3.66
    +
    +    - fixed testing issue
    +
     3.65
    
         - [Setup] include all files from twitter-bootstrap. closed #53

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -7,7 +7,7 @@ use Plack::Util ();
 use Carp ();
 use Amon2::Config::Simple;
 
-our $VERSION = '3.65';
+our $VERSION = '3.66';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
