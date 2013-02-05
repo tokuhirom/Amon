bf690721b99fb242853611a7f3499829409dc11d
tokuhirom <tokuhirom@gmail.com>
2013-02-05 12:43:03 +0900

    Checking in changes prior to tagging of version 3.67.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 695858c..5963396 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,8 @@
    +3.67
    +
    +    - eg/realtime-chat/chat.psgi: fix a destination to connect via web socket.(reported by shoutm)
    +    - upgrade to jQuery 1.9.0
    +
     3.66
    
         - fixed testing issue

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -7,7 +7,7 @@ use Plack::Util ();
 use Carp ();
 use Amon2::Config::Simple;
 
-our $VERSION = '3.66';
+our $VERSION = '3.67';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
