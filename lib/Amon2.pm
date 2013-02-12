af5b58d7bc8d444864ac6038ac912af76559dd70
tokuhirom <tokuhirom@gmail.com>
2013-02-12 18:08:20 +0900

    Checking in changes prior to tagging of version 3.71.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 51f4b58..609e751 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,11 @@
    +3.71 2013-02-12T18:04:33
    +
    +    [EXPERIMENTAL FEATURE]
    +    - Added Amon2::ContextGuard class.
    +      (tokuhirom)
    +    - Added Amon2::LocalContext class
    +      (tokuhirom)
    +
     3.70 2013-02-12T09:03:28
    
         [Web::Request]

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Carp ();
 use Amon2::Config::Simple;
 use Amon2::ContextGuard;
 
-our $VERSION = '3.70';
+our $VERSION = '3.71';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
