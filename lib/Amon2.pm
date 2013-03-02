497b9fed7a15375f4666e474ad756c16eeb4d26d
tokuhirom <tokuhirom@gmail.com>
2013-03-02 09:24:20 +0900

    Checking in changes prior to tagging of version 3.73.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2283462..524cb53 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,8 @@
    +3.73 2013-03-02T09:21:45
    +
    +    - [Setup] Set post_only attr for CSRFDefender, by default.
    +    - [Setup] upgrade jquery and twitter bootstrap
    +
     3.72 2013-02-13T08:51:52
    
         - Added Amon2#make_local_context() method.

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Carp ();
 use Amon2::Config::Simple;
 use Amon2::ContextGuard;
 
-our $VERSION = '3.72';
+our $VERSION = '3.73';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
