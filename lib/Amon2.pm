6f6ab076a5a4b7631303e577a06f5191a26573cf
tokuhirom <tokuhirom@gmail.com>
2013-03-29 09:50:16 +0900

    Checking in changes prior to tagging of version 3.78.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 5dcd3d7..3200d80 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,8 @@
    +3.78 2013-03-29T09:49:24
    +
    +    - Test case fixes
    +      (by hisaichi5518++)
    +
     3.77 2013-03-14T17:27:50
    
         - [Plugin::ShareDir] was split to another dist

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Carp ();
 use Amon2::Config::Simple;
 use Amon2::ContextGuard;
 
-our $VERSION = '3.77';
+our $VERSION = '3.78';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
