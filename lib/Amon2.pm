3b664f856269bd4560b65b49370a89df2ee5ecf6
tokuhirom <tokuhirom@gmail.com>
2013-04-18 18:13:26 +0900

    Checking in changes prior to tagging of version 3.79.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 3200d80..f5d23b8 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,10 @@
    +3.79 2013-04-18T18:08:44
    +
    +    - Write asset files with :raw.
    +      Older version of Amon2 write png files in ":encoding(utf-8)". orz.
    +      (Reported by geraud++)
    +    - Upgrade es5shim.js to latest.
    +
     3.78 2013-03-29T09:49:24
    
         - Test case fixes

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Carp ();
 use Amon2::Config::Simple;
 use Amon2::ContextGuard;
 
-our $VERSION = '3.78';
+our $VERSION = '3.79';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
