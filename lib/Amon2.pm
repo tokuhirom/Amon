44f17c62bf3c343c4d556b2685db5fa7b6e661ee
tokuhirom <tokuhirom@gmail.com>
2013-06-13 06:47:26 +0900

    Checking in changes prior to tagging of version 3.84.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6cd0794..79a83b4 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     {{$NEXT}}
    
    +3.84 2013-06-12T21:42:58Z
    +
    +    - Fixed testing deps
    +
     3.83 2013-06-01T05:14:35Z
    
         - fixed deps

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Carp ();
 use Amon2::Config::Simple;
 use Amon2::ContextGuard;
 
-our $VERSION = '3.83';
+our $VERSION = '3.84';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
