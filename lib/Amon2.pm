27870b3ec4a7afee7d5d828994fc8b2d43ad21b0
tokuhirom <tokuhirom@gmail.com>
2013-08-22 12:25:29 +0900

    Checking in changes prior to tagging of version 4.01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 3bb24f6..fd757c0 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,12 @@
     {{$NEXT}}
    
    +4.01 2013-08-22T03:23:21Z
    +
    +    - Fixed utf8 flag issue in uri_with().
    +      Before this version, if the key was flagged, it makes MOJIBAKE.
    +      https://github.com/tokuhirom/Amon/issues/63
    +      (Reported by ichigotake++)
    +
     4.00 2013-08-21T03:44:49Z
    
         - Bump to 4.00!

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Carp ();
 use Amon2::Config::Simple;
 use Amon2::ContextGuard;
 
-our $VERSION = '4.00';
+our $VERSION = '4.01';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
--- lib/Amon2/Setup/Flavor/Minimum.pm
+++ lib/Amon2/Setup/Flavor/Minimum.pm
@@ -14,7 +14,7 @@ use strict;
 use warnings;
 use utf8;
 use parent qw/Amon2/;
-our $VERSION='4.00';
+our $VERSION='4.01';
 use 5.008001;
 
 sub load_config {
