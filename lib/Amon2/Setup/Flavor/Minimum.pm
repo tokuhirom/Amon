5311fd4373fce920f6f113363646169c4d56b137
tokuhirom <tokuhirom@gmail.com>
2013-10-04 07:34:10 +0900

    Most of the people using Carton. We don't need to care the extlib/.

--- lib/Amon2/Setup/Flavor/Minimum.pm
+++ lib/Amon2/Setup/Flavor/Minimum.pm
@@ -255,7 +255,6 @@ use strict;
 use utf8;
 use File::Spec;
 use File::Basename;
-use lib File::Spec->catdir(dirname(__FILE__), 'extlib', 'lib', 'perl5');
 use lib File::Spec->catdir(dirname(__FILE__), 'lib');
 use Plack::Builder;
 ...
