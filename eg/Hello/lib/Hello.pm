07de4edc2554ab578f545d2b77b9856e7106b791
tokuhirom <tokuhirom@gmail.com>
2013-10-16 11:16:23 +0900

    Checking in changes prior to tagging of version 5.05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d83da74..6655482 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     {{$NEXT}}
    
    +5.05 2013-10-16T02:14:53Z
    +
    +    - RouterBoom: The path declared with get() function accepts 'HEAD' method.
    +      (tokuhirom)
    +
     5.04 2013-10-15T23:50:37Z
    
         - Make Router::Boom as a default routing engine.

--- eg/Hello/lib/Hello.pm
+++ eg/Hello/lib/Hello.pm
@@ -2,7 +2,7 @@ package Hello;
 use strict;
 use warnings;
 use parent qw/Amon2/;
-our $VERSION='5.04';
+our $VERSION='5.05';
 
 use Amon2::Config::Simple;
 sub load_config { Amon2::Config::Simple->load(shift) }
