cf57ede7265010a758331c24d2b3b6e78fa343da
magnolia <magnolia.k@me.com>
2013-10-24 22:30:44 +0900

    Fix contributor list.

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -285,26 +285,47 @@ Tokuhiro Matsuno E<lt>tokuhirom@gmail.comE<gt>
 =over 4
 
 =item noblejasper
+
 =item hiratara
+
 =item s-aska
+
 =item Kentaro Kuribayashi
+
 =item Yuki Ibe
+
 =item mattn
+
 =item Masahiro Nagano
+
 =item rightgo09
+
 =item karupanerura
+
 =item hatyuki
+
 =item Keiji, Yoshimi
+
 =item Nishibayashi Takuji
+
 =item dragon3
+
 =item Fuji, Goro
+
 =item issm
+
 =item hisaichi5518
+
 =item Adrian
+
 =item Fuji, Goro
+
 =item ITO Nobuaki
+
 =item Geraud CONTINSOUZAS
+
 =item Syohei YOSHIDA
+
 =item magnolia
 
 =back
