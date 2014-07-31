be11f57b818974844b29519caa7fae71aacd0809
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-08-01 06:40:52 +0900

    Checking in changes prior to tagging of version 6.04.
    Changelog diff is:
    
    diff --git Changes Changes
    index e827e4c..2bb3e2a 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,10 @@ Revision history for Perl module Amon2
    
     {{$NEXT}}
    
    +6.04 2014-07-31T21:40:18Z
    +
    +    - Make Amon2::Util::random_string() secure.
    +
     6.03 2014-07-30T07:39:34Z
    
         - Remove Amon2::Plugin::CSRFDefender from core dist.

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Carp ();
 use Amon2::Config::Simple;
 use Amon2::ContextGuard;
 
-our $VERSION = '6.03';
+our $VERSION = '6.04';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
