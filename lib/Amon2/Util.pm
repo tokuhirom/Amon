2fe09f2d6e0f3e679ceb1e2dfe4da5b3eae0ca45
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-08-01 06:37:56 +0900

    Make Amon2::Util::random_string() secure.

--- lib/Amon2/Util.pm
+++ lib/Amon2/Util.pm
@@ -3,6 +3,10 @@ use strict;
 use warnings;
 use base qw/Exporter/;
 use File::Spec;
+use MIME::Base64 ();
+use Digest::SHA ();
+use Time::HiRes;
+use POSIX ();
 
 our @EXPORT_OK = qw/add_method random_string/;
 
@@ -24,14 +28,33 @@ sub base_dir($) {
     }
 }
 
+our $URANDOM_FH;
+
+# $URANDOM_FH is undef if there is no /dev/urandom
+open $URANDOM_FH, '<:raw', '/dev/urandom'
+    or warn "Cannot open /dev/urandom: $!.";
+
 sub random_string {
-    my $length = shift;
-    my @chars = ( 'A'..'Z', 'a'..'z', '0'..'9' );
-    my $ret;
-    for (1..$length) {
-        $ret .= $chars[int rand @chars];
+    my $len = shift;
+
+    if ($URANDOM_FH) {
+        my $src_len = POSIX::ceil($len/3.0*4.0);
+        # Generate session id from /dev/urandom.
+        my $read = read($URANDOM_FH, my $buf, $src_len);
+        if ($read != $src_len) {
+            die "Cannot read bytes from /dev/urandom: $!";
+        }
+        my $result = MIME::Base64::encode_base64($buf, '');
+        $result =~ tr|+/=|\-_|d; # make it url safe
+        return substr($result, 0, $len);
+    } else {
+        # It's weaker than above. But it's portable.
+        my $out = '';
+        while (length($out) < $len) {
+            $out .= Digest::SHA::sha1_hex(rand() . $$ . {} . Time::HiRes::time());
+        }
+        return substr($out, 0, $len);
     }
-    return $ret;
 }
 
 1;
--- t/100_core/011_random_string.t
+++ t/100_core/011_random_string.t
@@ -4,6 +4,8 @@ use utf8;
 use Test::More;
 use Amon2::Util;
 
+diag sprintf("/dev/urandom: %s", $Amon2::Util::URANDOM_FH ? "available" : "unavailable");
+
 my $ret = Amon2::Util::random_string(32);
 is length($ret), 32;
 
@@ -11,6 +13,14 @@ is length($ret), 32;
     my $ret = join '', map { Amon2::Util::random_string(32) } 1..100;
     like $ret, qr/A/;
     like $ret, qr/9/;
+}
+
+for (1..100) {
+    is length(Amon2::Util::random_string($_)), $_;
+    {
+        local $Amon2::Util::URANDOM_FH;
+        is length(Amon2::Util::random_string($_)), $_;
+    }
 }
 
 done_testing;
