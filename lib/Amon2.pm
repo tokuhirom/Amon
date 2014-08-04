23ab43cfd2ee18d5e53c7943e6fd5d49969a35fe
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-08-04 12:54:55 +0900

    Checking in changes prior to tagging of version 6.08.
    Changelog diff is:
    
    diff --git Changes Changes
    index a69025e..0cc1293 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,10 @@ Revision history for Perl module Amon2
    
     {{$NEXT}}
    
    +6.08 2014-08-04T03:54:13Z
    +
    +    - Fixed test case
    +
     6.07 2014-08-01T19:32:01Z
    
         [Amon2::Util::random_string()]

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Carp ();
 use Amon2::Config::Simple;
 use Amon2::ContextGuard;
 
-our $VERSION = '6.07';
+our $VERSION = '6.08';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
