c57308f51bd36cd79165af77c86cef8d52a09200
Tokuhiro Matsuno <tokuhirom@gmail.com>
2026-02-27 10:14:14 +0900

    Checking in changes prior to tagging of version 6.17.
    Changelog diff is:
    
    diff --git Changes Changes
    index 93e41a1..df97480 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,8 @@ Revision history for Perl module Amon2
     
     {{$NEXT}}
     
    +6.17 2026-02-27T01:13:57Z
    +
         - Remove jshint test from generated projects
         - Remove unused Try::Tiny dependency
         - Drop Amon2::Plugin::Web::MobileAgent references and Extended example app

--- lib/Amon2.pm
+++ lib/Amon2.pm
@@ -8,7 +8,7 @@ use Carp ();
 use Amon2::Config::Simple;
 use Amon2::ContextGuard;
 
-our $VERSION = '6.16';
+our $VERSION = '6.17';
 {
     our $CONTEXT; # You can localize this variable in your application.
     sub context { $CONTEXT }
